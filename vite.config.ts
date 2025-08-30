25e112c3aadecaa831e9750c93dbbee5f35636cf
VasilGs <v.dostinovv@gmail.com>
2025-08-30 17:23:05 +0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/company/',              // ⬅️ важно: всички assets ще са /company/assets/...
-  resolve: {
-    alias: { '@': path.resolve(__dirname, './src') },
-  },
-  optimizeDeps: { exclude: ['lucide-react'] },
+  base: '/company/',            // ⬅️ важно за коректни пътища
 })
