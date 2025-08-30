6d30bd31768cefa3415ad569c1b8fec7034d00af
VasilGs <v.dostinovv@gmail.com>
2025-08-30 17:32:22 +0300

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { BASE } from './lib/url'
+const basename = BASE.replace(/\/$/, '')
 
 const root = document.getElementById('root')!
 createRoot(root).render(<App />)
